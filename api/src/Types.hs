18f55f99fc19d762d6c4abd9cb64026e58391b3c
Tomas Carnecky <tomas.carnecky@gmail.com>
2017-02-06 19:31:27 +0100

    Upgrade stack snapshot to nightly-2017-02-02

--- api/src/Types.hs
+++ api/src/Types.hs
@@ -10,6 +10,7 @@ module Types where
 
 import Control.Monad.IO.Class
 import Control.Monad.Trans.Either
+import Control.Monad.Except
 
 import Servant.Server
 
@@ -22,19 +23,19 @@ data AppState = AppState
     { _aversState :: Avers.Handle
     }
 
-reqAvers2 :: Avers.Handle -> Avers a -> EitherT ServantErr IO a
+reqAvers2 :: Avers.Handle -> Avers a -> Handler a
 reqAvers2 aversH m = do
     res <- liftIO $ evalAvers aversH m
     case res of
         Left e -> case e of
-            DatabaseError _detail                  -> left $ err500 -- $ InternalServerError $ "database " <> detail
-            NotAuthorized                          -> left $ err500 -- $ Unauthorized
-            DocumentNotFound _                     -> left $ err500 -- $ NotFound
-            UnknownObjectType _detail              -> left $ err500 -- $ InternalServerError $ "unknown object " <> detail
-            ObjectNotFound _                       -> left $ err500 -- $ NotFound
-            ParseError _ _detail                   -> left $ err500 -- $ InternalServerError $ "parse " <> detail
-            PatchError (UnknownPatchError _detail) -> left $ err500 -- $ InternalServerError $ "patch " <> detail
-            AversError _detail                     -> left $ err500 -- $ InternalServerError $ "avers " <> detail
-            InternalError _ie                      -> left err500 -- (Left ie)
+            DatabaseError _detail                  -> throwError err500 -- $ InternalServerError $ "database " <> detail
+            NotAuthorized                          -> throwError err500 -- $ Unauthorized
+            DocumentNotFound _                     -> throwError err500 -- $ NotFound
+            UnknownObjectType _detail              -> throwError err500 -- $ InternalServerError $ "unknown object " <> detail
+            ObjectNotFound _                       -> throwError err500 -- $ NotFound
+            ParseError _ _detail                   -> throwError err500 -- $ InternalServerError $ "parse " <> detail
+            PatchError (UnknownPatchError _detail) -> throwError err500 -- $ InternalServerError $ "patch " <> detail
+            AversError _detail                     -> throwError err500 -- $ InternalServerError $ "avers " <> detail
+            InternalError _ie                      -> throwError err500 -- (Left ie)
         Right r -> pure r
 
