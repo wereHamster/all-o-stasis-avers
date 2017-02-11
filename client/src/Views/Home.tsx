f6f5f3c9886b8a47efa29290d43115c2da8c9023
Tomas Carnecky <tomas.carnecky@gmail.com>
2017-02-11 17:35:01 +0100

    Insert heading separator when boulder createdAt day changes

--- client/src/Views/Home.tsx
+++ client/src/Views/Home.tsx
@@ -5,6 +5,7 @@ module Home
 */
 
 import * as React from 'react';
+import strftime from 'strftime';
 
 import * as Avers from 'avers';
 import Computation from 'computation';
@@ -40,13 +41,38 @@ const BoulderCardLoading = ({ app, boulderId }: { app: App, boulderId: string })
 
 export function
 homeView(app: App) {
-    const boulders = app.data.activeBouldersCollection.ids.get([]).map(boulderId => {
-        const boulderC = Avers.lookupEditable<Boulder>(app.data.aversH, boulderId);
+    // Get a list of all Editable<Boulder>, sort them by date.
+    // TODO: sort on the server.
+    const editableBoulders = app.data.activeBouldersCollection.ids.get([])
+        .map(boulderId => Avers.lookupEditable<Boulder>(app.data.aversH, boulderId).get(null))
+        .filter(x => x !== null)
+        .sort((a, b) => b.createdAt.getTime() - a.createdAt.getTime());
 
-        return boulderC.fmap((boulder) =>
-             <BoulderCard key={boulder.objectId} app={app} boulderE={boulder} />
-        ).get(<BoulderCardLoading app={app} boulderId={boulderId} />);
-    });
+    // Go through the list, render each boulder with <BoulderCard> and insert headings in
+    // between two cards when the day they were created at changes.
+    const {boulders} = editableBoulders.reduce(({boulders, date}, boulder) => {
+        const {objectId, createdAt} = boulder;
+
+        if (date === null) {
+            return {
+                boulders: boulders.concat([<BoulderCard key={objectId} app={app} boulderE={boulder} />]),
+                date: createdAt
+            };
+        } else if (date.getMonth() === createdAt.getMonth() && date.getDate() === createdAt.getDate()) {
+            return {
+                boulders: boulders.concat([<BoulderCard key={objectId} app={app} boulderE={boulder} />]),
+                date: createdAt
+            };
+        } else {
+            return {
+                boulders: boulders.concat(
+                    [ <div className="boulder-separator">{strftime('%A, %e. %B', createdAt)}</div>
+                    , <BoulderCard key={objectId} app={app} boulderE={boulder} />
+                    ]),
+                date: createdAt
+            };
+        }
+    }, { boulders: [], date: null });
 
     return (
         <Site app={app}>
