5b23d29f281937337324affc354939778bc32709
Tomas Carnecky <tomas.carnecky@gmail.com>
2017-09-16 14:54:26 +0200

    Clean up BoulderCard

--- client/src/Views/Components/BoulderCard.tsx
+++ client/src/Views/Components/BoulderCard.tsx
@@ -10,35 +10,37 @@ export interface BoulderCardProps {
     boulderE: Avers.Editable<Boulder>;
 }
 
-export class BoulderCard extends React.Component<BoulderCardProps, {}> {
+export class BoulderCard extends React.Component<BoulderCardProps> {
     onClick = () => {
-        navigateTo('/boulder/' + this.props.boulderE.objectId);
+        navigateTo('/boulder/' + this.props.boulderE.objectId)
     }
 
     render() {
-        const {app, boulderE} = this.props;
-        const {objectId, content} = boulderE;
-
-        const setters = content.setter.map(setterId => {
-            return Avers.lookupContent<Account>(app.data.aversH, setterId).fmap(account => (
-                <img className="boulder-card-setter" src={accountGravatarUrl(account.email)} />
-            )).get(undefined);
-        }).filter(x => x !== undefined);
-
-        const sectorName = prettyPrintSector(boulderE.content.sector);
+        const {app, boulderE} = this.props
+        const {objectId, content} = boulderE
+        const {grade, gradeNr, sector, setter} = content
 
         return (
             <div className="boulder-card" onClick={this.onClick}>
-                <div className={`boulder-card-id ${content.grade}`}>
-                    {boulderE.content.gradeNr}
+                <div className={`boulder-card-id ${grade}`}>
+                    {gradeNr}
                 </div>
                 <div className="boulder-card-meta">
-                    <div className="boulder-card-sector">{sectorName}</div>
+                    <div className="boulder-card-sector">{prettyPrintSector(sector)}</div>
                     <div className="boulder-card-setters">
-                        {setters}
+                        {setter.map(setterId => <BoulderCardSetter app={app} setterId={setterId} />)}
                     </div>
                 </div>
             </div>
         );
     }
 }
+
+
+const placeholderImageSrc = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAAAAACoWZBhAAAAF0lEQVQI12P4BAI/QICBFCaYBPNJYQIAkUZftTbC4sIAAAAASUVORK5CYII="
+const BoulderCardSetter = ({app, setterId}: {app: App, setterId: string}) => (
+    <img
+        className="boulder-card-setter"
+        src={Avers.lookupContent<Account>(app.data.aversH, setterId).fmap(account => accountGravatarUrl(account.email)).get(placeholderImageSrc)}
+    />
+)
