67e04e0ba7082407188611bad1226a30be4b0d65
Tomas Carnecky <tomas.carnecky@gmail.com>
2017-09-19 20:16:17 +0200

    Use styled-components to style BoulderCard

--- client/src/Views/Components/BoulderCard.tsx
+++ client/src/Views/Components/BoulderCard.tsx
@@ -1,13 +1,14 @@
-import * as Avers from 'avers';
-import * as React from 'react';
+import * as Avers from 'avers'
+import * as React from 'react'
+import styled from 'styled-components'
 
-import {accountGravatarUrl} from '../Account';
-import {App, navigateTo} from '../../app';
-import {Account, Boulder, prettyPrintSector} from '../../storage';
+import {accountGravatarUrl} from '../Account'
+import {App, navigateTo} from '../../app'
+import {Account, Boulder, prettyPrintSector} from '../../storage'
 
 export interface BoulderCardProps {
-    app: App;
-    boulderE: Avers.Editable<Boulder>;
+    app: App
+    boulderE: Avers.Editable<Boulder>
 }
 
 export class BoulderCard extends React.Component<BoulderCardProps> {
@@ -21,26 +22,165 @@ export class BoulderCard extends React.Component<BoulderCardProps> {
         const {grade, gradeNr, sector, setter} = content
 
         return (
-            <div className="boulder-card" onClick={this.onClick}>
-                <div className={`boulder-card-id ${grade}`}>
-                    {gradeNr}
-                </div>
-                <div className="boulder-card-meta">
-                    <div className="boulder-card-sector">{prettyPrintSector(sector)}</div>
-                    <div className="boulder-card-setters">
+            <Card onClick={this.onClick} grade={grade}>
+                <Id>{gradeNr}</Id>
+                <Meta>
+                    <Sector>{prettyPrintSector(sector)}</Sector>
+                    <Setters>
                         {setter.map(setterId => <BoulderCardSetter app={app} setterId={setterId} />)}
-                    </div>
-                </div>
-            </div>
-        );
+                    </Setters>
+                </Meta>
+            </Card>
+        )
     }
 }
 
-
 const placeholderImageSrc = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAAAAACoWZBhAAAAF0lEQVQI12P4BAI/QICBFCaYBPNJYQIAkUZftTbC4sIAAAAASUVORK5CYII="
 const BoulderCardSetter = ({app, setterId}: {app: App, setterId: string}) => (
-    <img
-        className="boulder-card-setter"
-        src={Avers.lookupContent<Account>(app.data.aversH, setterId).fmap(account => accountGravatarUrl(account.email)).get(placeholderImageSrc)}
-    />
+    <Setter src={Avers.lookupContent<Account>(app.data.aversH, setterId).fmap(account => accountGravatarUrl(account.email)).get(placeholderImageSrc)} />
 )
+
+
+
+// ----------------------------------------------------------------------------
+
+const gradeBackgroundColor = (grade: string) => {
+    switch (grade) {
+    case 'yellow': return '#F3D53F'
+    case 'green': return '#27AE60'
+    case 'orange': return '#E89C2F'
+    case 'blue': return '#1B69D2'
+    case 'red': return '#FF0000'
+    case 'white': return 'transparent'
+    }
+
+    return 'magenta'
+}
+
+const gradeBorderColor = (grade: string) => {
+    switch (grade) {
+    case 'yellow': return '#F3D53F'
+    case 'green': return '#27AE60'
+    case 'orange': return '#E89C2F'
+    case 'blue': return '#1B69D2'
+    case 'red': return '#FF0000'
+    case 'white': return 'transparent'
+    }
+
+    return 'magenta'
+}
+
+const gradeColor = (grade: string) => {
+    switch (grade) {
+    case 'yellow': return 'black'
+    case 'green': return 'white'
+    case 'orange': return 'black'
+    case 'blue': return 'white'
+    case 'red': return 'white'
+    case 'white': return 'black'
+    }
+
+    return 'magenta'
+}
+
+const Id = styled.div`
+    width: 40px;
+    height: 40px;
+    border-radius: 100%;
+    color: #444444;
+    background-color: magenta;
+    border: 2px solid magenta;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 26px;
+    line-height: 1;
+    box-shadow: 0 0 4px rgba(99, 85, 25, .5);
+    margin-right: 12px;
+    font-family: 'Advent Pro';
+    transition: box-shadow .2s;
+    flex: 0 0 40px;
+
+    @media (min-width: 600px) {
+        width: 64px;
+        height: 64px;
+        flex: 0 0 64px;
+        font-size: 48px;
+    }
+`
+
+const Card: any = styled.div`
+    display: flex;
+    flex-direction: row;
+    cursor: pointer;
+    padding: 8px 16px;
+    width: 100%;
+    overflow: hidden;
+
+    & ${Id} {
+        background-color: ${(props: any) => gradeBackgroundColor(props.grade)};
+        border-color: ${(props: any) => gradeBorderColor(props.grade)};
+        color: ${(props: any) => gradeColor(props.grade)};
+    }
+
+    &:hover ${Id} {
+        box-shadow: 0 0 8px rgba(99, 85, 25, .5);
+    }
+
+    @media (min-width: 600px) {
+        padding: 16px 24px;
+        width: calc(100% / 2);
+    }
+
+    @media (min-width: 900px) {
+        width: calc(100% / 3);
+    }
+
+    @media (min-width: 1200px) {
+        width: calc(100% / 4);
+    }
+
+`
+
+const Meta = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    flex: 1;
+
+    @media (min-width: 600px) {
+        flex-direction: column;
+        align-items: flex-start;
+        justify-content: center;
+    }
+`
+
+const Sector = styled.div`
+    text-transform: uppercase;
+    font-size: 22px;
+    line-height: 1;
+`
+
+const Setters = styled.div`
+    display: flex;
+    flex-direction: row;
+
+    @media (min-width: 600px) {
+        margin-top: 8px;
+    }
+`
+
+const Setter = styled.img`
+    width: 32px;
+    height: 32px;
+    margin: 0 0 0 4px;
+    display: block;
+    border-radius: 2px;
+
+    @media (min-width: 600px) {
+        width: 30px;
+        height: 30px;
+        margin: 0 4px 0 0;
+    }
+`
