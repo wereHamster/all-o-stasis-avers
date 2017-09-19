95aa63a149ccf618b0c97b8e1aa6eafad22bfc2a
Tomas Carnecky <tomas.carnecky@gmail.com>
2017-09-19 21:19:20 +0200

    BoulderSeparator

--- client/src/Views/Home.tsx
+++ client/src/Views/Home.tsx
@@ -4,15 +4,16 @@ module Home
 ) where
 */
 
-import * as Avers from 'avers';
-import * as React from 'react';
-import timeago from 'timeago.js';
+import * as Avers from 'avers'
+import * as React from 'react'
+import timeago from 'timeago.js'
+import styled from 'styled-components'
 
-import {App} from '../app';
-import {Boulder} from '../storage';
+import {App} from '../app'
+import {Boulder} from '../storage'
 
-import {BoulderCard} from './Components/BoulderCard';
-import {Site} from './Components/Site';
+import {BoulderCard} from './Components/BoulderCard'
+import {Site} from './Components/Site'
 
 const ThreeBounceSpinner = () => (
     <div className='sk-spinner sk-spinner-three-bounce'>
@@ -20,56 +21,55 @@ const ThreeBounceSpinner = () => (
         <div className='sk-bounce2'></div>
         <div className='sk-bounce3'></div>
     </div>
-);
+)
 
 export const LoadingTileBody = () => (
     <div className='boulder-card-body'>
         <ThreeBounceSpinner />
     </div>
-);
+)
 
-//{tileHeader(app, boulderId)}
 const BoulderCardLoading = ({ app, boulderId }: { app: App, boulderId: string }) => (
     <div key={boulderId} className={'boulder-card'}>
         <LoadingTileBody />
     </div>
-);
+)
 
 export function
 homeView(app: App) {
     const editableBoulders = app.data.activeBouldersCollection.ids.get([])
         .map(boulderId => Avers.lookupEditable<Boulder>(app.data.aversH, boulderId).get(null))
-        .filter(x => x !== null);
+        .filter(x => x !== null)
 
     // Go through the list, render each boulder with <BoulderCard> and insert
     // headings in between two cards when the day they were created at changes.
     const {boulders} = editableBoulders.reduce(({boulders, date}, boulder) => {
-        const objectId = boulder.objectId;
-        const createdAt = new Date(boulder.content.setDate);
+        const objectId = boulder.objectId
+        const createdAt = new Date(boulder.content.setDate)
 
         if (date === null) {
             return {
                 boulders: boulders.concat(
-                    [ <div className="boulder-separator">{timeago().format(createdAt)}</div>
+                    [ <BoulderSeparator>{timeago().format(createdAt)}</BoulderSeparator>
                     , <BoulderCard key={objectId} app={app} boulderE={boulder} />
                     ]),
                 date: createdAt
-            };
+            }
         } else if (date.getMonth() === createdAt.getMonth() && date.getDate() === createdAt.getDate()) {
             return {
                 boulders: boulders.concat([<BoulderCard key={objectId} app={app} boulderE={boulder} />]),
                 date: createdAt
-            };
+            }
         } else {
             return {
                 boulders: boulders.concat(
-                    [ <div className="boulder-separator">{timeago().format(createdAt)}</div>
+                    [ <BoulderSeparator>{timeago().format(createdAt)}</BoulderSeparator>
                     , <BoulderCard key={objectId} app={app} boulderE={boulder} />
                     ]),
                 date: createdAt
-            };
+            }
         }
-    }, { boulders: [], date: null });
+    }, { boulders: [], date: null })
 
     return (
         <Site app={app}>
@@ -77,5 +77,35 @@ homeView(app: App) {
                 {boulders}
             </div>
         </Site>
-    );
+    )
 }
+
+
+// ----------------------------------------------------------------------------
+const BoulderSeparator = styled.div`
+    flex: 0 0 100%;
+    width: 100%;
+    padding: 40px 16px 20px;
+    font-size: 24px;
+    line-height: 1;
+
+    &:first-of-type {
+        padding-top: 10px;
+    }
+
+    @media (min-width: 600px) {
+        padding: 80px 24px 32px;
+        font-size: 32px;
+        &:first-of-type {
+            padding: 20px 24px 32px;
+        }
+    }
+
+    @media (min-width: 1200px) {
+        padding: 120px 24px 60px;
+        font-size: 44px;
+        &:first-of-type {
+            padding: 30px 24px 60px;
+        }
+    }
+`
