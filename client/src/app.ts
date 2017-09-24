8f66873578402f1e2a0635d607c31a1d3bd272f6
Tomas Carnecky <tomas.carnecky@gmail.com>
2017-09-24 14:21:54 +0200

    Clean tslint warnings in app.ts

--- client/src/app.ts
+++ client/src/app.ts
@@ -9,75 +9,75 @@ module App
 ) where
 */
 
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
 
-import * as Avers from 'avers';
-import {Account, Boulder} from './storage';
+import * as Avers from 'avers'
+import {Account, Boulder} from './storage'
 
 import * as page from 'page'
 
-import configObject from './config';
+import configObject from './config'
 
 export class Config {
-    apiHost : string;
+    apiHost: string
 }
 
-Avers.definePrimitive(Config, 'apiHost', '//localhost:8000');
+Avers.definePrimitive(Config, 'apiHost', '//localhost:8000')
 
-export const config = Avers.mk<Config>(Config, configObject);
+export const config = Avers.mk<Config>(Config, configObject)
 
 export class App {
 
-    createBoulderPromise : Promise<string> = undefined;
+    createBoulderPromise: Promise<string> = undefined
 
-    constructor
-      ( public containerElement : Element
-      , public data             : Data
-      , public mkViewFn         : (app: App) => any
-      ) {}
+    constructor(
+        public containerElement: Element,
+        public data: Data,
+        public mkViewFn: (app: App) => any,
+    ) {}
 }
 
-export const infoTable = new Map<string, Avers.ObjectConstructor<any>>();
+export const infoTable = new Map<string, Avers.ObjectConstructor<any>>()
 
-infoTable.set('account',  Account);
-infoTable.set('boulder',  Boulder);
+infoTable.set('account',  Account)
+infoTable.set('boulder',  Boulder)
 
 export class Data {
 
-    session : Avers.Session;
+    session: Avers.Session
 
-    public accountsCollection     : Avers.ObjectCollection;
-    public adminAccountCollection : Avers.ObjectCollection;
+    public accountsCollection: Avers.ObjectCollection
+    public adminAccountCollection: Avers.ObjectCollection
 
-    public bouldersCollection       : Avers.ObjectCollection;
-    public activeBouldersCollection : Avers.ObjectCollection;
-    public ownedBoulderCollection   : Avers.ObjectCollection;
+    public bouldersCollection: Avers.ObjectCollection
+    public activeBouldersCollection: Avers.ObjectCollection
+    public ownedBoulderCollection: Avers.ObjectCollection
 
     constructor(public aversH: Avers.Handle) {
 
-        this.session = new Avers.Session(aversH);
+        this.session = new Avers.Session(aversH)
 
         // Collection of all accounts for user view
         // FIXME: where do we need the this collection??
         this.accountsCollection =
-            new Avers.ObjectCollection(aversH, 'accounts');
+            new Avers.ObjectCollection(aversH, 'accounts')
 
         // Collection of admin/setter accounts (display team)
         this.adminAccountCollection =
-            new Avers.ObjectCollection(aversH, 'adminAccounts');
+            new Avers.ObjectCollection(aversH, 'adminAccounts')
 
         // Collecion of all boulders to compute statistics
         this.bouldersCollection =
-            new Avers.ObjectCollection(aversH, 'boulders');
+            new Avers.ObjectCollection(aversH, 'boulders')
 
         // Collection of all boulders currently active in the gym
         this.activeBouldersCollection =
-            new Avers.ObjectCollection(aversH, 'activeBoulders');
+            new Avers.ObjectCollection(aversH, 'activeBoulders')
 
         // Collection of boulders for a given user (setter/admin)
         this.ownedBoulderCollection =
-            new Avers.ObjectCollection(aversH, 'ownedBoulders');
+            new Avers.ObjectCollection(aversH, 'ownedBoulders')
     }
 }
 
@@ -91,17 +91,17 @@ refresh(app: App): void {
 
 export function
 loadView(app: App, mkViewFn: (app: App) => React.ReactElement<any>): void {
-    app.mkViewFn = mkViewFn;
-    refresh(app);
+    app.mkViewFn = mkViewFn
+    refresh(app)
 }
 
 export function
-navigateTo(p : string) {
-    page(p);
+navigateTo(p: string) {
+    page(p)
 }
 
 export function
 navigateToFn(p: string) {
-    return () => { navigateTo(p); }
+    return () => { navigateTo(p) }
 }
 
