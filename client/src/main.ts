57e7f09cc33fc50b241b0c8395fe8c44883e392c
Tomas Carnecky <tomas.carnecky@gmail.com>
2017-09-24 14:29:20 +0200

    Fix tslint warnings in main.ts

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,119 +1,120 @@
-import * as React from 'react';
+import * as React from 'react'
 import * as page from 'page'
 
-import * as Avers from 'avers';
-import {Data, App, config, infoTable, refresh, loadView} from './app';
+import * as Avers from 'avers'
+import {Data, App, config, infoTable, refresh, loadView} from './app'
 
-import {loadingView, notFoundView} from './views';
-import {accountView} from './Views/Account';
-import {boulderView} from './Views/Boulder';
-import {homeView}    from './Views/Home';
-import {loginView}   from './Views/Login';
-import {sectorView}  from './Views/Sector';
-import {teamView}    from './Views/Team';
-import {signupView}  from './Views/Signup';
-import {catalogView} from './Views/Catalog';
-import {updateSecretView}   from './Views/UpdateSecret';
+import {loadingView, notFoundView} from './views'
+import {accountView} from './Views/Account'
+import {boulderView} from './Views/Boulder'
+import {homeView} from './Views/Home'
+import {loginView} from './Views/Login'
+import {sectorView} from './Views/Sector'
+import {teamView} from './Views/Team'
+import {signupView} from './Views/Signup'
+import {catalogView} from './Views/Catalog'
+import {updateSecretView} from './Views/UpdateSecret'
 
 
-function mkApp(): App {
-    let aversH = new Avers.Handle
-        ( config.apiHost
-        , (<any>window).fetch.bind(window)
-        , path => new WebSocket('ws:' + config.apiHost + path)
-        , window.performance.now.bind(window.performance)
-        , infoTable
-        );
+const mkApp = (): App => {
+    const aversH = new Avers.Handle(
+        config.apiHost,
+        window.fetch.bind(window),
+        path => new WebSocket('ws:' + config.apiHost + path),
+        window.performance.now.bind(window.performance),
+        infoTable,
+    )
 
-    let data = new Data(aversH);
+    const data = new Data(aversH)
 
-    return new App
-        ( document.getElementById('root')
-        , data
-        , loadingView
-        );
+    return new App(
+        document.getElementById('root'),
+        data,
+        loadingView,
+    )
 }
 
-const main = function() {
-    console.info('Starting app...');
+const main = () => {
+    // console.info('Starting app...')
 
     // Create the application instance. Pass all required configuration to the
     // constructor.
-    let app = mkApp();
+    const app = mkApp()
 
     // Attach a listener to watch for changes. Refresh the application UI
     // when any change happens. This is the main rendering loop of the
     // application. The 'console.info' shows you how frequently the application
     // data changes.
     Avers.attachGenerationListener(app.data.aversH, () => {
-        // console.info('Generation', app.data.aversH.generationNumber);
-        refresh(app);
-    });
+        // console.info('Generation', app.data.aversH.generationNumber)
+        refresh(app)
+    })
 
     // This template uses page.js for the router. If you target modern browsers
     // you may get away with a more straightforward implementation and listen
     // to the hashchange events directly.
-    setupRoutes(app);
+    setupRoutes(app)
 
     // The app template makes use of the Avers Session. First thing we try to
     // do is to restore an existing session. The views will use the information
     // that is stored in the session object to determine what to show.
-    Avers.restoreSession(app.data.session);
+    Avers.restoreSession(app.data.session)
 
     // Expose some useful tools on 'window' to make them easily accessible from
     // the developer console. This part is entirely optional, though
     // I recommend to keep it even in production. It doesn't add any significant
     // overhead and gives you easy access to the application state.
-    (<any>window).Avers = Avers;
-    (<any>window).app   = app;
+    const windowAny = window as any
+    windowAny.Avers = Avers
+    windowAny.app = app
 }
 
 
 function setupRoutes(app: App) {
     page('/', () => {
         loadView(app, homeView)
-    });
+    })
 
-    page('/signup', function() {
+    page('/signup', () => {
         loadView(app, signupView)
-    });
+    })
 
-    page('/login', function() {
+    page('/login', () => {
         loadView(app, loginView)
-    });
+    })
 
-    page('/sector', function(ctx) {
-        loadView(app, sectorView);
-    });
+    page('/sector', ctx => {
+        loadView(app, sectorView)
+    })
 
-    page('/boulder/:boulderId', function(ctx) {
+    page('/boulder/:boulderId', ctx => {
         loadView(app, boulderView(ctx.params.boulderId))
-    });
+    })
 
-    page('/account/:accountId', function(ctx) {
+    page('/account/:accountId', ctx => {
         loadView(app, accountView(ctx.params.accountId))
-    });
+    })
 
-    page('/account/:accountId/updateSecret', function(ctx) {
+    page('/account/:accountId/updateSecret', ctx => {
         loadView(app, updateSecretView(ctx.params.accountId))
-    });
+    })
 
-    page('/team', function() {
+    page('/team', () => {
         loadView(app, teamView)
-    });
+    })
 
-    page('/_catalog', function() {
+    page('/_catalog', () => {
         loadView(app, catalogView)
-    });
-    page('/_catalog/*', function() {
+    })
+    page('/_catalog/*', () => {
         loadView(app, catalogView)
-    });
+    })
 
     page('*', () => {
         loadView(app, notFoundView)
-    });
+    })
 
-    page();
+    page()
 }
 
 main()
