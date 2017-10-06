f22bb1147475511f3bf4eb753fac7f06c3c7480d
iff <iff@yvesineichen.com>
2017-10-06 13:19:26 +0200

    client: major css cleanup, inline styles for team and settercard

--- client/src/Views/Components/SetterCard.tsx
+++ client/src/Views/Components/SetterCard.tsx
@@ -1,5 +1,6 @@
 import * as Avers from 'avers'
 import * as React from 'react'
+import styled from 'styled-components'
 
 import {accountGravatarUrl} from '../Account'
 import {App, navigateToFn} from '../../app'
@@ -12,12 +13,27 @@ export interface SetterCardProps {
 }
 
 export const SetterCard = ({app, accountId, account}: SetterCardProps) => (
-    <div className='setter-card'>
+    <Setter>
         <div onClick={navigateToFn('/account/' + accountId)}>
             <img className='avatar' src={account ? accountGravatarUrl(account.email) : placeholderImageSrc}/>
             <div>{(account && account.name !== '') ? account.name : accountId.slice(0, 2)}</div>
         </div>
-    </div>
+    </Setter>
 )
 
 const placeholderImageSrc = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAAAAACoWZBhAAAAF0lEQVQI12P4BAI/QICBFCaYBPNJYQIAkUZftTbC4sIAAAAASUVORK5CYII='
+
+const Setter = styled.div`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    text-align: center;
+    margin-left: 10px;
+    margin-right: 10px;
+
+    img {
+        height: 50px;
+        border: 1px solid #999;
+        border-radius: 50%;
+    }
+`
--- client/src/Views/Team.tsx
+++ client/src/Views/Team.tsx
@@ -1,37 +1,47 @@
-/*
-module Team
-( teamView
-) where
-*/
+import * as Avers from 'avers'
+import * as React from 'react'
+import styled from 'styled-components'
 
-import * as Avers from 'avers';
-import * as React from 'react';
+import {App} from '../app'
+import {Account} from '../storage'
 
-import {App} from '../app';
-import {Account} from '../storage';
-
-import {Site} from './Components/Site';
-import {SetterCard} from './Components/SetterCard';
+import {Site} from './Components/Site'
+import {SetterCard} from './Components/SetterCard'
 
 export function
 teamView(app: App) {
     const setters = app.data.adminAccountCollection.ids.get([]).map(accountId => {
-        const accountC = Avers.lookupContent<Account>(app.data.aversH, accountId);
+        const accountC = Avers.lookupContent<Account>(app.data.aversH, accountId)
         return accountC.fmap(account =>
-            <SetterCard key={accountId} app={app} accountId={accountId} account={account} />
-        ).get(<SetterCard key={accountId} app={app} accountId={accountId} account={undefined} />);
-    });
+            <SetterCard key={accountId} app={app} accountId={accountId} account={account} />,
+        ).get(<SetterCard key={accountId} app={app} accountId={accountId} account={undefined} />)
+    })
 
     return (
       <Site app={app}>
-        <div className='team'>
-          <div className="logo">
-            Team
-          </div>
-          <div className="members">
-            {setters}
-          </div>
-        </div>
+        <Root>
+          <Logo>Team</Logo>
+          <Members>{setters}</Members>
+        </Root>
       </Site>
-    );
+    )
 }
+
+const Root = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`
+
+const Logo = styled.div`
+    text-align: center;
+    font-size: 4rem;
+    font-family: "trajan-sans-pro";
+    margin-top: 4rem;
+`
+const Members = styled.div`
+    padding-top: 80px;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+`
