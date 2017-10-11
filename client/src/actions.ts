8b87404d4b8b0a5eb6b8192260eb5706bbca0176
iff <iff@yvesineichen.com>
2017-10-11 18:07:07 +0200

    client:actions: make linter happy, go directly to new boulder when created

--- client/src/actions.ts
+++ client/src/actions.ts
@@ -1,55 +1,50 @@
-/*
-module Actions
-( createBoulder
-, role
-) where
-*/
+import * as Avers from 'avers'
+import * as Storage from './storage'
+import {App, navigateTo, refresh} from './app'
+import {Account} from './storage'
 
-import * as Avers from 'avers';
-import * as Storage from './storage';
-import {App, navigateTo, refresh} from './app';
-import {Account} from './storage';
-
-function mkBoulder(app: App) : Storage.Boulder {
-    const now = Date.now();
+function mkBoulder(app: App): Storage.Boulder {
+    const now = Date.now()
     return Avers.mk(Storage.Boulder,
         { setter : [app.data.session.objId],
-          setDate : now
-        }
-    );
+          setDate : now,
+        },
+    )
 }
 
 export function
 createBoulder(app: App) {
-    let promise = Avers.createObject(app.data.aversH, 'boulder', mkBoulder(app)).then(id => {
-        app.createBoulderPromise = undefined;
-        Avers.resetObjectCollection(app.data.ownedBoulderCollection);
-        navigateTo('/');
-        return id;
-    });
+    const promise = Avers.createObject(app.data.aversH, 'boulder', mkBoulder(app)).then(id => {
+        app.createBoulderPromise = undefined
+        Avers.resetObjectCollection(app.data.ownedBoulderCollection)
+        refresh(app)
+        navigateTo('/boulder/' + id)
+        return id
+    })
 
-    app.createBoulderPromise = promise;
-    refresh(app);
+    app.createBoulderPromise = promise
+    refresh(app)
 
-    return promise;
+    return promise
 }
 
 export function
-role(app: App) : string {
-    let accountC = Avers.lookupContent<Account>(app.data.aversH, app.data.session.objId);
-    let accountE = accountC.get(null);
+role(app: App): string {
+    const accountC = Avers.lookupContent<Account>(app.data.aversH, app.data.session.objId)
+    const accountE = accountC.get(null)
 
-    if (accountE === null)
-        return "user";
-    else
-        return accountE.role;
+    if (accountE === null) {
+        return 'user'
+    } else {
+        return accountE.role
+    }
 }
 
 // filter on the client
 export function
-sectorBoulders(app: App, sectorName: String) : Avers.Editable<Storage.Boulder>[] {
+sectorBoulders(app: App, sectorName: string): Array<Avers.Editable<Storage.Boulder>> {
     return app.data.activeBouldersCollection.ids.get([])
         .map(boulderId => Avers.lookupEditable<Storage.Boulder>(app.data.aversH, boulderId).get(null))
         .filter(x => x !== null)
-        .filter(x => x.content.sector == sectorName);
+        .filter(x => x.content.sector === sectorName)
 }
