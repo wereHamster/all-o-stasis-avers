c46cd4dbc641da1c82c62e824e015f00c2619979
iff <iff@yvesineichen.com>
2017-11-18 09:51:22 +0100

    client::signup: lint

--- client/src/Views/Signup.tsx
+++ client/src/Views/Signup.tsx
@@ -4,88 +4,89 @@ module Signup
 ) where
 */
 
-import * as Avers from 'avers';
-import * as React from 'react';
+import * as Avers from 'avers'
+import * as React from 'react'
 
-import {App, navigateToFn} from '../app';
+import {App, navigateToFn} from '../app'
 
-import {Site} from './Components/Site';
+import {Site} from './Components/Site'
 
-var signupInProgress = false;
+let signupInProgress = false
 
 export function
 signupView(app: App) {
 
     function doSignUp() {
         return e => {
-            e.stopPropagation();
+            e.stopPropagation()
 
-            signupInProgress = true;
-            Avers.startNextGeneration(app.data.aversH);
+            signupInProgress = true
+            Avers.startNextGeneration(app.data.aversH)
 
-            var signupStartedAt = Date.now();
+            const signupStartedAt = Date.now()
 
             Avers.signup(app.data.session, '').then(accountId => {
-                return Avers.signin(app.data.session, accountId, '');
+                return Avers.signin(app.data.session, accountId, '')
             }).then(() => {
-                var signupDuration = Date.now() - signupStartedAt;
-                var delay = Math.max(0, 3000 - signupDuration);
+                const signupDuration = Date.now() - signupStartedAt
+                const delay = Math.max(0, 3000 - signupDuration)
                 setTimeout(() => {
-                    signupInProgress = false;
-                    Avers.startNextGeneration(app.data.aversH);
-                }, delay);
+                    signupInProgress = false
+                    Avers.startNextGeneration(app.data.aversH)
+                }, delay)
             }).catch(err => {
-                alert('Sign up failed: ' + err);
-            });
-        };
+                alert('Sign up failed: ' + err)
+            })
+        }
     }
 
     if (app.data.session.objId && signupInProgress === false) {
         return (
             <Site app={app}>
-              <div className="login">
-                <div className="logo">All-o-stasis</div>
-                <div className="about">
+              <div className='login'>
+                <div className='logo'>All-o-stasis</div>
+                <div className='about'>
                     We generated a unique identifier for your account.
                     Write it down somewhere, you will need it to log in again.
                     If you forget it it you will lose access all-o-stasis.
                     You have been warned.
                 </div>
-                <div className="about">
+                <div className='about'>
                     In order to be able to login with a username/password,
                     see the account prefences. Note that until you set a
                     password, your account is not protected.
                 </div>
-                <div className="button" onClick={navigateToFn('/')}>
+                <div className='button' onClick={navigateToFn('/')}>
                   Alright, I understand. Now let me continue already.
                 </div>
               </div>
             </Site>
-        );
+        )
     } else {
-        var btn, login;
+        let btn
+        let login
         if (signupInProgress) {
             btn = (
-                <div className="signup-in-progress">
-                  <div className="text">
+                <div className='signup-in-progress'>
+                  <div className='text'>
                     We are busy opening a new account for you.
                   </div>
                 </div>
-            );
+            )
         } else {
-            btn = <div className="button" onClick={doSignUp()}>Sign up</div>;
-            login = <div className="button" onClick={navigateToFn('/login')}>I already have an account (login)</div>;
+            btn = <div className='button' onClick={doSignUp()}>Sign up</div>
+            login = <div className='button' onClick={navigateToFn('/login')}>I already have an account (login)</div>
         }
 
         return (
             <Site app={app}>
-              <div className="login">
-                <div className="logo">All-o-stasis</div>
-                <div className="about">
+              <div className='login'>
+                <div className='logo'>All-o-stasis</div>
+                <div className='about'>
                     Signing up is as easy as clicking the button below.
                     We don't ask for any personal identifying information.
                 </div>
-                <div className="about">
+                <div className='about'>
                     Once your account has been created you can provide
                     additional information at your discression.
                 </div>
@@ -93,6 +94,6 @@ signupView(app: App) {
                 {login}
               </div>
             </Site>
-        );
+        )
     }
 }
