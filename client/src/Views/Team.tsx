f49301d5362282454eb1d409b7f1e1132e64a154
Tomas Carnecky <tomas.carnecky@gmail.com>
2018-10-06 14:07:51 +0200

    Use grid layout for the team page

--- client/src/Views/Team.tsx
+++ client/src/Views/Team.tsx
@@ -61,4 +61,8 @@ export default class extends React.Component<{ app: App }> {
 
 const Root = styled.div`
   margin: 0 24px;
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+  grid-gap: 30px 30px;
+  justify-items: stretch;
 `;
