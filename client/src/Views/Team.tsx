231f1f0c7b6ae87fa4525ebb28e053a7e8e18812
Tomas Carnecky <tomas.carnecky@gmail.com>
2018-10-06 13:59:59 +0200

    Show only active setters on the team page

--- client/src/Views/Team.tsx
+++ client/src/Views/Team.tsx
@@ -1,26 +1,62 @@
 import * as Avers from "avers";
 import * as React from "react";
+import Computation from "computation";
 import styled from "styled-components";
 
 import { App } from "../app";
-import { Account } from "../storage";
+import { Account, Boulder } from "../storage";
 
 import { Site } from "./Components/Site";
 import { SetterBlock } from "../Components/SetterBlock";
 
-export default function teamView({ app }: { app: App }) {
-  const setters = app.data.adminAccountCollection.ids.get([]).map(accountId => {
-    const accountC = Avers.lookupContent<Account>(app.data.aversH, accountId);
-    return accountC
-      .fmap(account => <SetterBlock key={accountId} app={app} accountId={accountId} account={account} />)
-      .get(<SetterBlock key={accountId} app={app} accountId={accountId} account={undefined} />);
-  });
+export default class extends React.Component<{ app: App }> {
+  render() {
+    const { app } = this.props;
 
-  return (
-    <Site app={app}>
-      <Root>{setters}</Root>
-    </Site>
-  );
+    // This code is very inefficient because we have to fetch the content(!)
+    // of all admin accounts and active boulders on the client to determine
+    // which setters are active (have boulders on the wall).
+    const activeSetters = Computation.liftA2(
+      app.data.activeBouldersCollection.ids,
+      app.data.adminAccountCollection.ids,
+      (activeBoulderIds, adminAccountsIds) => {
+        const accounts: Array<{ accountId: string; account: Account }> = [];
+
+        const activeSetterIds = new Set<string>();
+        activeBoulderIds.forEach(boulderId => {
+          Avers.lookupContent<Boulder>(app.data.aversH, boulderId)
+            .fmap(boulder => {
+              boulder.setter.forEach(setterId => {
+                activeSetterIds.add(setterId);
+              });
+            })
+            .get(undefined);
+        });
+
+        adminAccountsIds.forEach(accountId => {
+          if (activeSetterIds.has(accountId)) {
+            Avers.lookupContent<Account>(app.data.aversH, accountId)
+              .fmap(account => {
+                accounts.push({ accountId, account });
+              })
+              .get(undefined);
+          }
+        });
+
+        return accounts;
+      }
+    ).get([]);
+
+    return (
+      <Site app={app}>
+        <Root>
+          {activeSetters.map(({ accountId, account }) => (
+            <SetterBlock key={accountId} app={app} accountId={accountId} account={account} />
+          ))}
+        </Root>
+      </Site>
+    );
+  }
 }
 
 const Root = styled.div`
