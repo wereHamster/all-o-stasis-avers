7c67d960355840cfd7d9f94a511ec2043e027021
Tomas Carnecky <tomas.carnecky@gmail.com>
2018-10-14 19:21:25 +0200

    Rename Setters to Team in the navbar

--- client/src/Components/NavBar.tsx
+++ client/src/Components/NavBar.tsx
@@ -37,7 +37,7 @@ const HomeItem = () => (
 
 const TeamItem = () => (
   <Item isActive={window.location.pathname === "/team"} onClick={navigateToFn("/team")}>
-    Setters
+    Team
   </Item>
 );
 
