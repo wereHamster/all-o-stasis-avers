94ae042fe5fbde8c29122849df53710e668e0237
Tomas Carnecky <tomas.carnecky@gmail.com>
2018-11-10 10:54:39 +0100

    Reformat SetterSelector

--- client/src/Views/Components/Stats/SetterSelector.tsx
+++ client/src/Views/Components/Stats/SetterSelector.tsx
@@ -1,60 +1,71 @@
-import * as Avers from 'avers'
-import * as React from 'react'
-import styled from 'styled-components'
+import * as Avers from "avers";
+import * as React from "react";
+import styled from "styled-components";
 
-import {Account} from '../../../storage'
-import {App, activeSetters} from '../../../app'
-import {accountAvatar} from '../../Account'
+import { Account } from "../../../storage";
+import { App, activeSetters } from "../../../app";
+import { accountAvatar } from "../../Account";
 
-import {text, lightGrey} from '../../../Materials/Colors'
-import {useTypeface, copy14} from '../../../Materials/Typefaces'
+import { text, lightGrey } from "../../../Materials/Colors";
+import { useTypeface, copy14 } from "../../../Materials/Typefaces";
 
-import {Section, SectionLink} from './Internal'
-
+import { Section, SectionLink } from "./Internal";
 
 export interface SetterSelectorProps {
-    app: App
-    selectedSetters: string[]
+  app: App;
+  selectedSetters: string[];
 
-    clear: undefined | (() => void)
-    toggle(setterId: string): void
+  clear: undefined | (() => void);
+  toggle(setterId: string): void;
 }
 
-export const SetterSelector = ({app, selectedSetters, clear, toggle}: SetterSelectorProps) => {
-    const setters = activeSetters(app).get<string[]>([]).map(accountId => {
-        return Avers.lookupContent<Account>(app.data.aversH, accountId).fmap(account => (
-            <Setter
-                key={accountId}
-                app={app}
-                accountId={accountId}
-                account={account}
-                toggle={toggle}
-                isSelected={selectedSetters.length === 0 || selectedSetters.indexOf(accountId) !== -1}
-            />
+export const SetterSelector = ({ app, selectedSetters, clear, toggle }: SetterSelectorProps) => {
+  const setters = activeSetters(app)
+    .get<string[]>([])
+    .map(accountId => {
+      return Avers.lookupContent<Account>(app.data.aversH, accountId)
+        .fmap(account => (
+          <Setter
+            key={accountId}
+            app={app}
+            accountId={accountId}
+            account={account}
+            toggle={toggle}
+            isSelected={selectedSetters.length === 0 || selectedSetters.indexOf(accountId) !== -1}
+          />
         ))
-        .get(<Setter key={accountId} app={app} accountId={accountId} account={undefined} toggle={toggle} isSelected={false} />)
-    })
+        .get(
+          <Setter
+            key={accountId}
+            app={app}
+            accountId={accountId}
+            account={undefined}
+            toggle={toggle}
+            isSelected={false}
+          />
+        );
+    });
 
-    return (
-        <>
-            <Section>
-                Setter
-                <SectionLink onClick={clear}>(reset)</SectionLink>
-            </Section>
+  return (
+    <>
+      <Section>
+        Setter
+        <SectionLink onClick={clear}>(reset)</SectionLink>
+      </Section>
 
-            <div>
-                {setters}
-            </div>
-        </>
-    )
-}
+      <div>{setters}</div>
+    </>
+  );
+};
 
-const Setter = ({app, accountId, account, toggle, isSelected}) => (
-    <SetterC onClick={() => toggle(accountId)}>
-        <SetterImage isSelected={isSelected} src={accountAvatar(app.data.aversH, accountId)} />
-        <SetterName isSelected={isSelected}>{(account && account.name !== '') ? account.name : accountId.slice(0, 7)}</SetterName>
-    </SetterC>
-)
+const Setter = ({ app, accountId, account, toggle, isSelected }) => (
+  <SetterC onClick={() => toggle(accountId)}>
+    <SetterImage isSelected={isSelected} src={accountAvatar(app.data.aversH, accountId)} />
+    <SetterName isSelected={isSelected}>
+      {account && account.name !== "" ? account.name : accountId.slice(0, 7)}
+    </SetterName>
+  </SetterC>
+);
 
 const SetterC = styled.div`
 display: flex;
@@ -68,9 +79,9 @@ user-select: none;
 transition all .2s;
 
 &:hover {
-    border-left-color: ${text};
+  border-left-color: ${text};
 }
-`
+`;
 
 const SetterImage = styled<{ isSelected: boolean }, "img">("img")`
 display: block;
@@ -80,11 +91,11 @@ margin-right: 8px;
 border-radius: 2px;
 
 transition all .2s;
-opacity: ${({isSelected}) => isSelected ? 1 : 0.2}
-`
+opacity: ${({ isSelected }) => (isSelected ? 1 : 0.2)}
+`;
 
 const SetterName = styled<{ isSelected: boolean }, "div">("div")`
-${useTypeface(copy14)}
-transition all .2s;
-color: ${({isSelected}) => isSelected ? text : lightGrey}
-`
+  ${useTypeface(copy14)};
+  transition all .2s;
+  color: ${({ isSelected }) => (isSelected ? text : lightGrey)};
+`;
