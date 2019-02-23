3e96b92fcb6857c34b53633af099508b6335f75a
Tomas Carnecky <tomc@caurea.org>
2019-02-23 15:47:51 +0100

    Hide BoulderFilter from the main screen on mobile

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -146,10 +146,11 @@ const BoulderGradeToggleButton = ({ grade, grades, onToggle }) => (
 
 // ----------------------------------------------------------------------------
 const BoulderFilter = styled.div`
-  display: flex;
+  display: none;
   align-items: flex-start;
   padding: 20px 16px 12px;
   @media (min-width: 600px) {
+    display: flex;
     padding: 20px 24px 20px;
   }
 `;
